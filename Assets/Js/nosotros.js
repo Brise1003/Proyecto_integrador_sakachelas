bcaf16261c35b01eb0780437c0070b12fc923f07
MartinRoblesJ97 <martin.alejandro.robles.juarez@gmail.com>
2022-09-10 19:20:37 -0500

    Se actualiza pagina Nosotros con carrusel y estilos funcionales, falta descripción y foto de algunos integrantes

---
+++ Assets/Js/nosotros.js
@@ -0,0 +1,29 @@
+// CARRUSEL
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel_lista'), {
+        rewind: true,
+        draggable: true,
+		dots: '.carousel_indicadores',
+		arrows: {
+			prev: '.carousel_anterior',
+			next: '.carousel_siguiente'
+		},
+		responsive: [
+			{
+			  breakpoint: 576,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			  }
+			},
+            {
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+});
