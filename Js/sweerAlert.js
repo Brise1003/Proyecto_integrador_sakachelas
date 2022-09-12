2553bc2b04d4949ba851d67b911179a050818bcd
AndreaLPG <lizand.pg@gmail.com>
2022-09-12 14:50:12 -0500

    nueva tercera act

---
+++ Js/sweerAlert.js
@@ -0,0 +1,16 @@
+
+//confirmación de edad 
+swal.fire({
+    title: "Uso de cookies",
+    confirmButtonText: "Aceptado"
+    
+})
+
+//aviso de privacidad
+swal.fire({
+    title:"Aviso de privacidad",
+    html: "<p>Teléfonos de la oficina de privacidad: 555-55 </p> <br> Correo electrónico: ventas@cervexxa.mx <br> <b>Definiciones</b>. <br>Datos personales: Cualquier información concerniente a una persona física identificada o identificable. <br>Titular: La persona física (TITULAR) a quien identifica o corresponden los datos personales. <br>Responsable: Persona física o moral de carácter privado que decide sobre el tratamiento de los datos personales. <br>Tratamiento: La obtención, uso (que incluye el acceso, manejo, aprovechamiento, transferencia o disposición de datos personales), divulgación o almacenamiento de datos personales por cualquier medio. <br>Transferencia: Toda comunicación de datos realizada a persona distinta del responsable o encargado del tratamiento.<br>Derechos ARCO: Derechos de acceso, rectificación, cancelación y oposición. <br>Consentimiento Tácito: Se entenderá que el titular ha consentido en el tratamiento de los datos, cuando habiéndose puesto a su disposición el Aviso de Privacidad, no manifieste su oposición.<br><b>Fines de la información.</b> <br>Sus datos personales serán utilizados para los siguientes fines:<br>Fines de marketing <br>Fines de contacto <br>Fines para conocer requerimientos del proyecto",
+    width:`85%`
+})
+     
+
