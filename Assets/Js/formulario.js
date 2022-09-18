c6086759583d32b11cbf61ac528d19b2da9f470c
Fernando-Fl0res <fernando.herrera2207@gmail.com>
2022-09-18 16:39:38 -0500

    Se acomodan los botones de mostrar contraseña

--- Assets/Js/formulario.js
+++ Assets/Js/formulario.js
@@ -1,13 +1,15 @@
-const formulario = document.getElementById('singUp');
-const inputs = document.querySelectorAll('#singUp input')
+const formulario = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input')
 
  const  expresiones  =  {
- 	usuario : /^[a-zA-Z0-9\_\-]{ 4,16}$/,  // Letras, numeros, guion y guion_bajo
+ 	// usuario : /^[a-zA-Z0-9\_\-]{ 4,16}$/,  // Letras, numeros, guion y guion_bajo
  	nombre : /^[a-zA-ZÀ-ÿ\s]{1,40}$/,  // Letras y espacios, pueden llevar acentos.
+  apellido : /^[a-zA-ZÀ-ÿ\s]{1,40}$/,  // Letras y espacios, pueden llevar acentos.
  	contraseña : /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,  // 4 a 12 dígitos.
  	correo : /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
- 	telefono : /^\d{7,14}$/  // 7 a 14 numeros.
+ 	// telefono : /^\d{7,14}$/  // 7 a 14 numeros.
  }
+ 
 
  const campos = {
   nombre: false,
@@ -22,7 +24,7 @@ const validarFormulario = (e) => {
       validarCampo(expresiones.nombre, e.target, 'nombre');
     break;
     case "apellido":
-      validarCampo(expresiones.nombre, e.target, 'apellido');
+      validarCampo(expresiones.apellido, e.target, 'apellido');
     break;
     // case "fecha":
       
@@ -93,8 +95,11 @@ formulario.addEventListener('submit', (e) =>{
   if(campos.nombre && campos.apellido && campos.correo && campos.password /*&& conf.cheked*/){
   formulario.reset();
 
-  // document.getElementById('form__mensaje-exito').classList.add('form__mensaje-exito-activo');
+  
+ document.getElementById('form__mensaje-exito').classList.add('form__mensaje-exito-activo');
 
+}else{
+  document.getElementById('form__mensaje').classList.add('form__mensaje-activo');
 }
 });
 
@@ -159,17 +164,13 @@ formulario.addEventListener('submit', (e) =>{
       }
     } 
     
-
- 
-
-
   function mostrarPassword2(){
-      var cambio = document.getElementById("password2");
-      if(cambio.type == "password"){
-        cambio.type = "text";
+      var cambio2 = document.getElementById("password2");
+      if(cambio2.type == "password"){
+        cambio2.type = "text";
         $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
       }else{
-        cambio.type = "password";
+        cambio2.type = "password";
         $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
       }
     } 
